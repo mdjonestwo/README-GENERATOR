108c31aae6a8d085299e17ef6a2d166becff5387
mdjonestwo <mdjones2@protonmail.com>
2020-11-18 23:54:20 -0500

    answers populated

--- index.js
+++ index.js
@@ -5,12 +5,85 @@ inquirer
     .prompt([
         {
             type: 'input',
-            message: 'Title?',
+            message: 'Title',
             name: "title" 
+        },
+        {
+            type: 'input',
+            message: 'Description',
+            name: "description" 
+        },
+        {
+            type: 'input',
+            message: 'Installation Instructions',
+            name: "install" 
+        },
+        {
+            type: 'input',
+            message: 'Usage Information?',
+            name: "usage" 
+        },    
+        {
+            type: 'input',
+            message: 'Contribution Guidelines',
+            name: "guidelines" 
+        },
+        {
+            type: 'input',
+            message: 'Test Instructions',
+            name: "test" 
+        },    
+        {
+            type: 'list',
+            message: 'License',
+            name: "license", 
+            choices: ['MIT', 'YOU', "LUV"]
+        },
+        {
+            type: 'input',
+            message: 'GitHub Username',
+            name: "github",     
+        },
+        {
+            type: 'input',
+            message: 'Email',
+            name: "email", 
         }
     ])
     .then((response) => {
-        fs.writeFile('log.git adtxt', JSON.stringify(response), {}, (e) =>
+        const filename = `${response.title.toLowerCase().split(' ').join("-")}.md`
+        function Data(title, description, install, usage, guidelines, test, license){
+            this.title = title
+            this.description = description
+            this.install = install
+            this.usage = usage
+            this.guidelines = guidelines
+            this.test = test
+            this.license = license
+            this.getDetails = function () {
+                return "## TITLE" + this.title;
+            }
+        }
+        const newData = new Data(response.title)
+       
+
+        const foo =
+        `## TITLE
+        ${response.title}
+        ## DESCRIPTION 
+        ${response.description}
+        ## INSTALLATION INSTRUCTIONS 
+        ${response.install}
+        ## USAGE INFORMATION 
+        ${response.usage}
+        ## CONTRIBUTION GUIDELINES 
+        ${response.guidelines}
+        ## TEST INSTRUCTIONS
+        ${response.test}
+        ## LICENSE
+        ${response.license}`
+        
+        fs.writeFile(filename, foo, {}, (e) =>
         console.log(e)
         )
     })
---
+++ llol.md
@@ -0,0 +1,14 @@
+## TITLE
+        llol
+        ## DESCRIPTION 
+        asd
+        ## INSTALLATION INSTRUCTIONS 
+        sd
+        ## USAGE INFORMATION 
+        aWF
+        ## CONTRIBUTION GUIDELINES 
+        WAF
+        ## TEST INSTRUCTIONS
+        AWF
+        ## LICENSE
+        MIT
